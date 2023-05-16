b764a1cf33eedaa70b88901bbefab2b7faa97c48
yusufobr <yousarsif80@gmail.com>
2023-05-16 14:33:51 +0100

    add remove item, toggle amount & calculate totals

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,23 +1,43 @@
-import React from 'react'
-import {ChevronUp, ChevronDown} from '../icons'
+import React from "react";
+import { ChevronUp, ChevronDown } from "../icons";
+import { removeItem, increase, decrease } from "../features/cart/cartSlice";
+import { useDispatch } from "react-redux";
 
-
-const CartItem = ({id, img, title, price, amount}) => {
+const CartItem = ({ id, img, title, price, amount }) => {
+  const dispatch = useDispatch();
   return (
-    <article className='cart-item'>
+    <article className="cart-item">
       <img src={img} alt={title} />
       <div>
         <h4>{title}</h4>
-        <h4 className='item-price'>{price}</h4>
-        <button className='remove-btn'>remove</button>
+        <h4 className="item-price">{price}</h4>
+        <button onClick={() => dispatch(removeItem(id))} className="remove-btn">
+          remove
+        </button>
       </div>
-      <di>
-        <button className='amount-btn'><ChevronUp /></button>
-        <p className='amount'>{amount}</p>
-        <button className='amount-btn'><ChevronDown /></button>
-      </di>
+      <div>
+        <button
+          className="amount-btn"
+          onClick={() => dispatch(increase({ id }))}
+        >
+          <ChevronUp />
+        </button>
+        <p className="amount">{amount}</p>
+        <button
+          className="amount-btn"
+          onClick={() => {
+            if (amount === 1) {
+              dispatch(removeItem(id));
+              return;
+            }
+            dispatch(decrease({ id }));
+          }}
+        >
+          <ChevronDown />
+        </button>
+      </div>
     </article>
-  )
-}
+  );
+};
 
-export default CartItem
+export default CartItem;
